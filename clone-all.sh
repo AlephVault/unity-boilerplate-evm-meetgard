e1c5ac67888db45e6f931a37f50b2acacb215441
Luis Masuelli <luis@datagran.io>
2023-11-03 11:34:26 -0500

    Updating the dependencies

--- clone-all.sh
+++ clone-all.sh
@@ -10,8 +10,10 @@ git clone --branch 0.0.4 git@github.com:AlephVault/unity-meetgard-auth.git ../un
 git clone --branch 0.0.3 git@github.com:AlephVault/unity-meetgard-scopes.git ../unity-meetgard-scopes
 git clone --branch 0.0.2 git@github.com:AlephVault/unity-remotestorage.git ../unity-remotestorage
 git clone --branch 0.0.1 git@github.com:AlephVault/unity-ipfs.git ../unity-ipfs
+git clone --branch 0.0.1 git@github.com:AlephVault/unity-deeplinks.git ../unity-deeplinks
 git clone --branch 0.0.2 git@github.com:AlephVault/unity-evmgames.git ../unity-evmgames
 git clone --branch 0.0.3 git@github.com:AlephVault/unity-evmgames-auth.git ../unity-evmgames-auth
 git clone --branch 0.0.2 git@github.com:AlephVault/unity-evmgames-livecache.git ../unity-evmgames-livecache
 git clone --branch 0.0.1 git@github.com:AlephVault/unity-evmgames-localstorage.git ../unity-evmgames-localstorage
+git clone --branch 0.0.1 git@github.com:AlephVault/unity-evmgames-deeplinks.git ../unity-evmgames-deeplinks
 
